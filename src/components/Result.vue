d3dd008ef5ce46a86490fd18e8b60518a45e31fb
Hao Ding <dh@mwc-006204.dhcp.missouri.edu>
2019-02-01 20:13:30 -0600

    show result

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -2,14 +2,14 @@
   <div>
     Result
     <div>
-      <li v-for="people in peopleJson.peoplelist" :key="people"><span>Name : </span>{{people.fullname}}</li>
+      <li v-for="people in peoplelist" :key="people"><span>Name : </span>{{people.id}}</li>
 
     </div>
     <div>
       *******************************************
     </div>
     <div>
-      <li v-for="publication in publicationJson.publicationlist" :key="publication">
+      <li v-for="publication in publicationlist" :key="publication">
       <p>{{publication.mainTitle}}</p>
       <p><span>Authors : </span><span v-for="author in publication.authorsIds" :key="author">{{author}}, </span></p>
       <p><span>Abstract : </span>{{publication.abstractContent}}</p>
@@ -23,149 +23,106 @@ import axios from 'axios'
 export default {
   name: 'Result',
   mounted () {
-    // alert(this.$route.query.searchtext)
-    // this.url = 'http://35.247.68.0:8080/api/test/search/people/0/$(this.$route.query.searchtext)'
     if (this.$route.query.searchrange === '') {
       this.url = 'http://35.247.68.0:8080/api/test/search/0/' + this.$route.query.searchtext
+      axios.get(this.url).then(
+        response => {
+          this.peoplelist = response.data.data.people
+          this.publicationlist = response.data.data.publication
+        }
+      ).catch(error => {
+        console.log(error)
+      })
     } else if (this.$route.query.searchrange === 'people') {
       this.url = 'http://35.247.68.0:8080/api/test/search/people/0/' + this.$route.query.searchtext
+      axios.get(this.url).then(
+        response => {
+          this.peoplelist = response.data.data
+        }
+      ).catch(error => {
+        console.log(error)
+      })
     } else {
       this.url = 'http://35.247.68.0:8080/api/test/search/publication/0/' + this.$route.query.searchtext
+      axios.get(this.url).then(
+        response => {
+          this.publicationlist = response.data.data
+        }
+      ).catch(error => {
+        console.log(error)
+      })
     }
-    axios.get(this.url).then(
-      response => {
-        console.log(response.data)
-      }
-    ).catch(error => {
-      console.log(error)
-    })
   },
   data () {
     return {
       url: '',
-      peopleJson: {
-        'peoplelist': [{
-          'career': [
-            'string'
-          ],
-          'email': 'string',
-          'firstname': 'string',
-          'fullname': 'string',
-          'id': 'string',
-          'imageLargeUrl': 'string',
-          'imageMediumUrl': 'string',
-          'imageSmailUrl': 'string',
-          'imageUrl': 'string',
-          'information': 'string',
-          'isUser': true,
-          'lastname': 'string',
-          'organization': [
-            'string'
-          ],
-          'phonenumber': 'string',
-          'userId': 'string'
-        }
-        ]
-      },
-      publicationJson: {
-        'publicationlist': [{
-          'id': '12345',
-          'otherId': {
-            'MOSPACE': '1234123',
-            'WIKI': '345634563',
-            'IEEE': '12312341'
-          },
-          'mainTitle': 'Main Title',
-          'subTitle': 'Sub Title',
-          'abstractContent': 'Abstract Content',
-          'content': 'Some Content',
-          'contentFileIds': [
-            '1',
-            '2',
-            '3'
-          ],
-          'references': [
-            'A Reference',
-            'Other'
-          ],
-          'originUrl': 'https://www.google.com',
-          'citation': [
-            'A Citation',
-            'Other'
-          ],
-          'collections': [
-            'A Collection',
-            'Other'
-          ],
-          'authorsIds': [
-            '1',
-            '2',
-            '3'
-          ],
-          'publishDate': '2018-10-07T20:51:40.343+0000',
-          'submitDate': '2018-10-07T20:51:40.343+0000',
-          'publisher': [
-            'A Publisher',
-            'Other'
-          ],
-          'subjectIds': [
-            '1',
-            '2',
-            '3'
-          ],
-          'language': 'English',
-          'publicationType': 'PublicationType'
-        },
-        {
-          'id': '12345',
-          'otherId': {
-            'MOSPACE': '1234123',
-            'WIKI': '345634563',
-            'IEEE': '12312341'
-          },
-          'mainTitle': 'Main Title',
-          'subTitle': 'Sub Title',
-          'abstractContent': 'Abstract Content',
-          'content': 'Some Content',
-          'contentFileIds': [
-            '1',
-            '2',
-            '3'
-          ],
-          'references': [
-            'A Reference',
-            'Other'
-          ],
-          'originUrl': 'https://www.google.com',
-          'citation': [
-            'A Citation',
-            'Other'
-          ],
-          'collections': [
-            'A Collection',
-            'Other'
-          ],
-          'authorsIds': [
-            '1',
-            '2',
-            '3'
-          ],
-          'publishDate': '2018-10-07T20:51:40.343+0000',
-          'submitDate': '2018-10-07T20:51:40.343+0000',
-          'publisher': [
-            'A Publisher',
-            'Other'
-          ],
-          'subjectIds': [
-            '1',
-            '2',
-            '3'
-          ],
-          'language': 'English',
-          'publicationType': 'PublicationType'
-        }
-        ]
+      peoplelist: [{
+        'career': [
+          'string'
+        ],
+        'email': '',
+        'firstname': '',
+        'fullname': '',
+        'id': '',
+        'imageLargeUrl': '',
+        'imageMediumUrl': '',
+        'imageSmailUrl': '',
+        'imageUrl': '',
+        'information': '',
+        'isUser': true,
+        'lastname': '',
+        'organization': [
+          ''
+        ],
+        'phonenumber': '',
+        'userId': ''
       }
+      ],
+
+      publicationlist: [{
+        'id': '',
+        'otherId': {
+          'MOSPACE': '',
+          'WIKI': '',
+          'IEEE': ''
+        },
+        'mainTitle': 'Main Title',
+        'subTitle': 'Sub Title',
+        'abstractContent': 'Abstract Content',
+        'content': 'Some Content',
+        'contentFileIds': [
+          ''
+        ],
+        'references': [
+          'A Reference',
+          'Other'
+        ],
+        'originUrl': 'https://www.google.com',
+        'citation': [
+          'A Citation',
+          'Other'
+        ],
+        'collections': [
+          'A Collection',
+          'Other'
+        ],
+        'authorsIds': [
+          '1'
+        ],
+        'publishDate': '',
+        'submitDate': '',
+        'publisher': [
+          'A Publisher',
+          'Other'
+        ],
+        'subjectIds': [
+          ''
+        ],
+        'language': 'English',
+        'publicationType': 'PublicationType'
+      }
+      ]
+
     }
   }
 }
