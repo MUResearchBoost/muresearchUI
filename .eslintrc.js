afdd1e4af118a163e6c4e16aace42cbb53c2ded4
HuiMing Sun <bamboo1886@gmail.com>
2019-05-01 19:32:02 -0500

    add deploy docker-compose

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
   ],
   rules: {
     'indent': ['error', 2],
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
+    //'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
